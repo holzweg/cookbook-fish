9c3fd61b837f00c2ce73292930fa4baebe38defb
Workflow <florian.peter@gmx.at>
2014-01-24 12:57:37 +0100

    Add berksfile, specify ubuntu support.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,9 @@ maintainer       'Gabor Pihaj'
 license          'MIT'
 description      'Installs/Configures fish shell'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+
 depends          'build-essential'
 depends          'git'
+
+supports         'ubuntu'
